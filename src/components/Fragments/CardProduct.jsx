1ff27fcb48f472028c2387f1e7e132c9a6ae7734
imam.basri <imam.basri@edi-indonesia.co.id>
2024-01-08 13:39:43 +0700

    Update From NVIM
    Update 9 Episode

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className='w-full max-w-sm bg-gray-500 border border-gray-400 rounded-lg shadow'>
+    <div className="w-full h-[600px] max-w-sm bg-gray-500 border border-gray-400 rounded-lg shadow">
       {children}
     </div>
   );
@@ -12,18 +12,22 @@ const CardProduct = (props) => {
 const Header = (props) => {
   const { images, alt = "images" } = props;
   return (
-    <a href='#'>
-      <img src={images} alt={alt} className='p-8 rounded-t-lg' />
+    <a href="#">
+      <img src={images} alt={alt} className="p-8 rounded-t-lg" />
     </a>
   );
 };
 const Body = (props) => {
-  const { children, title } = props;
+  const { children, name } = props;
   return (
-    <div class='px-5 pb-5'>
-      <a href='#'>
-        <h5 className='text-xl  font-semibold tracking-tight text-white'>{title}</h5>
-        <p className='text-md text-white'>{children}</p>
+    <div class="px-5 pb-5">
+      <a href="#">
+        <h5 className="text-xl font-semibold tracking-tight text-white">
+          {name}
+        </h5>
+        <p className="text-md text-white overflow-y-auto h-[200px]">
+          {children}
+        </p>
       </a>
     </div>
   );
@@ -32,9 +36,14 @@ const Body = (props) => {
 const Footer = (props) => {
   const { price } = props;
   return (
-    <div className='flex items-center justify-between px-5 py-5'>
-      <span className='text-xl font-bold text-white'>{price}</span>
-      <Button type='submit' variant='bg-blue-600' hover='hover:bg-blue-800' width='w-50%'>
+    <div className="flex items-center justify-between px-5 py-5">
+      <span className="text-xl font-bold text-white">{price}</span>
+      <Button
+        type="submit"
+        variant="bg-blue-600"
+        hover="hover:bg-blue-800"
+        width="w-50%"
+      >
         Add To Cart
       </Button>
     </div>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,28 +1,49 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
 const ProductsPage = () => {
+  const products = [
+    {
+      id: 1,
+      name: "Sepatu Baru",
+      price: "Rp. 100.000",
+      image: "/images/product1.jpg",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam hic, aspernatur dolore cumque
+          ratione earum magni porro vel neque quo esse excepturi eaque totam molestias est nihil
+          rerum consectetur maiores quos quod labore! Accusantium obcaecati autem id error laborum
+          non inventore nemo, cumque iure veritatis. Velit dignissimos quos unde obcaecati!
+`,
+    },
+    {
+      id: 2,
+      name: "Sepatu Lama",
+      price: "Rp.200.000",
+      image: "/images/product1.jpg",
+      description: `ratione earum magni porro vel neque quo esse excepturi eaque totam molestias est nihil
+          rerum consectetur maiores quos quod labore! Accusantium obcaecati autem id error laborum
+          non inventore nemo, cumque iure veritatis. Velit dignissimos quos unde obcaecati!
+`,
+    },
+    {
+      id: 3,
+      name: "Sepatu Lama",
+      price: "Rp.200.000",
+      image: "/images/product1.jpg",
+      description: "Sepatu Baru Kita Jual Dengan Harga Murah",
+    },
+  ];
   return (
-    <div className='flex justify-center items-center h-screen py-5 gap-5'>
-      <CardProduct>
-        <CardProduct.Header images='/images/product1.jpg' />
-        <CardProduct.Body title='Laptop Baru'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam hic, aspernatur dolore cumque
-          ratione earum magni porro vel neque quo esse excepturi eaque totam molestias est nihil
-          rerum consectetur maiores quos quod labore! Accusantium obcaecati autem id error laborum
-          non inventore nemo, cumque iure veritatis. Velit dignissimos quos unde obcaecati!
-        </CardProduct.Body>
-        <CardProduct.Footer price='Rp. 1.000.000' />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header images='/images/product1.jpg' />
-        <CardProduct.Body title='Laptop Baru'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam hic, aspernatur dolore cumque
-          ratione earum magni porro vel neque quo esse excepturi eaque totam molestias est nihil
-          rerum consectetur maiores quos quod labore! Accusantium obcaecati autem id error laborum
-          non inventore nemo, cumque iure veritatis. Velit dignissimos quos unde obcaecati!
-        </CardProduct.Body>
-        <CardProduct.Footer price='Rp. 1.000.000' />
-      </CardProduct>
+    <div className="flex justify-center h-[600px] py-5 gap-5">
+      {products.map((product) => {
+        return (
+          <CardProduct>
+            <CardProduct.Header images={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        );
+      })}
     </div>
   );
 };
