56a2bb6ff24592ccffe46efdadf7dda048524be0
imam.basri <imam.basri@edi-indonesia.co.id>
2024-01-14 17:25:38 +0700

    Update 14Eps - menambahkan Cart update UseEffect

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 
@@ -40,10 +40,22 @@ const ProductsPage = () => {
     },
   ];
 
-  const [cart, setCart] = useState([{
-    id: 1,
-    qty: 1
-  }])
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0)
+
+  useEffect(()=>{
+    setCart(JSON.parse(localStorage.getItem("cart") || []))
+  },[])
+  useEffect(()=>{
+  if(cart.length > 0 ){
+    const sum = cart.reduce((acc, item) => {
+      const product = products.find((product) => product.id === item.id);
+      return acc + product.price * item.qty;
+    }, 0);
+    setTotalPrice(sum);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }  
+  },[cart])
   const email = localStorage.getItem("email");
   const handleLogout = () => {
     localStorage.removeItem("email");
@@ -52,10 +64,10 @@ const ProductsPage = () => {
   };
 
   const handleAddToCart = (id)=>{
-    if(cart.find(item => item.id === id)){
-      setCart(cart.map(item => item.id === id ? {...item, qty: item.qty + 1} : item))
-    }else{
-      setCart([...cart, {id, qty: 1}])
+    if (cart.find((item) => item.id === id)) {
+      setCart(cart.map((item) => (item.id === id ? { ...item, qty: item.qty + 1 } : item)));
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
     }
   }
   return (
@@ -108,6 +120,10 @@ const ProductsPage = () => {
                   </tr>
                 );
               })}
+              <tr>
+                <td colSpan={3}><b>Total Price</b></td>
+                <td><b>Rp.{(totalPrice).toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}</b></td>
+              </tr>
             </tbody>
           </table>
         </div>
