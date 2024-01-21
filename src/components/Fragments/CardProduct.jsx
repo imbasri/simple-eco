8954ae47f5a25b2dcb90f05ed2cce2560ee20f20
imam.basri <imam.basri@edi-indonesia.co.id>
2024-01-22 00:00:19 +0700

    update Eps21 - implementasi redux

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Button from "../Elements/Button";
-
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/slices/cartSlice";
 const CardProduct = (props) => {
   const { children } = props;
   return (
@@ -11,10 +12,14 @@ const CardProduct = (props) => {
 };
 
 const Header = (props) => {
-  const { images, alt = "images" ,id} = props;
+  const { images, alt = "images", id } = props;
   return (
     <Link to={`/product/${id}`}>
-      <img src={images} alt={alt} className="p-8 rounded-t-lg bg-contain aspect-auto  w-full h-[300px]" />
+      <img
+        src={images}
+        alt={alt}
+        className="p-8 rounded-t-lg bg-contain aspect-auto  w-full h-[300px]"
+      />
     </Link>
   );
 };
@@ -24,10 +29,10 @@ const Body = (props) => {
     <div className="px-5">
       <a href="#">
         <h5 className="text-xl font-semibold tracking-tight text-white">
-          {name.substring(0,20)}...
+          {name.substring(0, 20)}...
         </h5>
         <p className="text-md text-white overflow-y-auto text-wrap w-full h-[200px] p-2">
-          {children.substring(0,200)}...
+          {children.substring(0, 200)}...
         </p>
       </a>
     </div>
@@ -35,16 +40,20 @@ const Body = (props) => {
 };
 
 const Footer = (props) => {
-  const { price,handleAddToCart ,id} = props;
+  const { price, handleAddToCart, id } = props;
+  const dispath = useDispatch();
   return (
     <div className="flex items-center justify-between px-4">
-      <span className="text-xl font-bold text-white">${price.toLocaleString('id-ID',{styles: 'currency', currency: 'USD'})}</span>
+      <span className="text-xl font-bold text-white">
+        $
+        {price.toLocaleString("id-ID", { styles: "currency", currency: "USD" })}
+      </span>
       <Button
         type="submit"
         variant="bg-blue-600"
         hover="hover:bg-blue-800"
         width="w-50%"
-        onClick={()=>handleAddToCart(id)}
+        onClick={() => dispath(addToCart({ id, qty: 1 }))}
       >
         Add To Cart
       </Button>
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import ErrorPage from "./pages/404.jsx";
 import ProductsPage from "./pages/products.jsx";
 import ProfilePage from "./pages/profile.jsx";
 import DetailProductPage from "./pages/detailProduct.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -44,6 +46,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
